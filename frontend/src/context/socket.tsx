7a4fe60f437dad12102d2645a52cf588f297e56a
Anchit Sinha <anchit1909@gmail.com>
2024-07-13 10:10:00 +0530

    feat[frontend]: create context for socket

---
+++ frontend/src/context/socket.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { io, Socket } from "socket.io-client";
+
+interface SocketContextType {
+  socket: Socket | null;
+}
+
+const SocketContext = createContext<SocketContextType | null>(null);
+
+export const useSocket = () => {
+  const context = useContext(SocketContext);
+  if (!context) {
+    throw new Error("useSocket must be used within a SocketProvider");
+  }
+  return context.socket;
+};
+
+interface SocketProviderProps {
+  children: ReactNode;
+}
+
+export const SocketProvider = ({ children }: SocketProviderProps) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const connection = new (io as any)("http://localhost:3001", {
+      withCredentials: true,
+      extraHeaders: {
+        "my-custom-header": "abcd",
+      },
+    });
+    console.log("socket connection", connection);
+    setSocket(connection);
+
+    connection.on("connect_error", (err: any) => {
+      console.log("Error establishing socket", err);
+    });
+
+    return () => {
+      connection.close();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
